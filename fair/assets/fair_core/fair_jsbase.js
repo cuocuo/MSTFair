b46b50d3daee51aaa494241c12fb683c638c34ce
shiheng <shiheng@mistong.com>
2022-08-26 14:36:10 +0800

    fair_jsbase文件bigint代码注释

--- fair/assets/fair_core/fair_jsbase.js
+++ fair/assets/fair_core/fair_jsbase.js
@@ -1853,35 +1853,35 @@ Object.__inner__ = function () {};
   };
   global.String = String;
 })();
-(function () {
-  BigInt.one = 1;
-  BigInt.two = 2;
-  BigInt.zero = 0;
+// (function () {
+//   BigInt.one = 1;
+//   BigInt.two = 2;
+//   BigInt.zero = 0;
 
-  BigInt.from = function (num) {
-    // 取整数部分
-    return parseInt(num);
-  };
+//   BigInt.from = function (num) {
+//     // 取整数部分
+//     return parseInt(num);
+//   };
 
-  BigInt.parse = function (str) {
-    // 只接受数字字符串，小数会报错
-    if (str.indexOf(".") == -1 && !isNaN(Number(str))) {
-      return BigInt(str);
-    } else {
-      throw "FormatException: Could not parse BigInt";
-    }
-  };
+//   BigInt.parse = function (str) {
+//     // 只接受数字字符串，小数会报错
+//     if (str.indexOf(".") == -1 && !isNaN(Number(str))) {
+//       return BigInt(str);
+//     } else {
+//       throw "FormatException: Could not parse BigInt";
+//     }
+//   };
 
-  BigInt.tryParse = function (str) {
-    // 只接受数字字符串，小数会报错
-    if (str.indexOf(".") == -1 && !isNaN(Number(str))) {
-      return BigInt(str);
-    } else {
-      return null;
-    }
-  };
-  global.BigInt = BigInt;
-})();
+//   BigInt.tryParse = function (str) {
+//     // 只接受数字字符串，小数会报错
+//     if (str.indexOf(".") == -1 && !isNaN(Number(str))) {
+//       return BigInt(str);
+//     } else {
+//       return null;
+//     }
+//   };
+//   global.BigInt = BigInt;
+// })();
 (function () {
   const NativeSymbol = global.Symbol;
 
